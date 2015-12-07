8c12b331156be3dcdf0d0ea6d81278ad573dfc0a
rhutnyk_bo <roman.hutnyk@bitsorchestra.com>
2015-12-07 17:28:42 -0500

    Instantiated consumer object.

--- Integration/ConsumeRESTServiceOnServer/CallRestConsumerSample.cs
+++ Integration/ConsumeRESTServiceOnServer/CallRestConsumerSample.cs
@@ -4,6 +4,7 @@ public partial class CMSWebParts_TestWebParts_RestTest : System.Web.UI.UserContr
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        var userData = RESTConsumer.GetUsersFeed("http://localhost:80/", "RestClient", "secretpassword");
+        var consumer = new RESTConsumer();
+        var userData = consumer.GetUsersFeed("http://localhost:80/", "RestClient", "secretpassword");
     }
 }
